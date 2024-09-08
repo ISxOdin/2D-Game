83856367556c901c1d847436192668ccc5abe305
ISxOdin <ilias.strub@gmail.com>
2024-09-08 14:52:41 +0400

    Collision

--- 01_2D_Game/src/entity/Player.java
+++ 01_2D_Game/src/entity/Player.java
@@ -1,6 +1,7 @@
 package entity;
 
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
@@ -23,6 +24,13 @@ public class Player extends Entity{
 		
 		screenX = gp.screenWidth/2 - (gp.tileSize/2);
 		screenY = gp.screenHeight/2 - (gp.tileSize/2);
+	
+		//COLLISIONAREA
+		solidArea = new Rectangle();
+		solidArea.x = 8;
+		solidArea.y = 16;
+		solidArea.width = 32;
+		solidArea.height = 32;
 		
 		setDefaultValues();
 		getPlayerImage();
@@ -30,8 +38,8 @@ public class Player extends Entity{
 	public void setDefaultValues() {
 		
 		//STARTING POSITION
-		worldX = gp.tileSize * 4; 
-		worldY = gp.tileSize * 4;
+		worldX = gp.tileSize * 24; 
+		worldY = gp.tileSize * 24;
 		speed = 4;
 		direction = "down";
 	}
@@ -59,17 +67,28 @@ public class Player extends Entity{
 			
 			if(keyH.upPressed == true) {
 				direction = "up";
-				worldY -= speed;
 			} else if (keyH.downPressed == true) {
 				direction = "down";
-				worldY += speed;
 			} else if (keyH.leftPressed == true) {
 				direction = "left";
-				worldX -= speed;
 			} else if (keyH.rightPressed == true) {
 				direction = "right";
-				worldX += speed;
 			}
+			
+			//CHECK TILE COLLISION
+			collisionOn = false;
+			gp.cCheck.checkTile(this);
+			
+			//IF COLLISION IS FALSE, PLAYER CAN MOVE
+			if(collisionOn == false) {
+				switch(direction) {
+				case "up": worldY -= speed; break;
+				case "down": worldY += speed; break;
+				case "left": worldX -= speed; break;
+				case "right": worldX += speed; break;
+				}
+			}
+			
 			spriteCounter++;
 			if(spriteCounter > 12) {
 				if(spriteNum == 1) {
